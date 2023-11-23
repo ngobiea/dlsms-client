3d004519bb0a98f6518b59b5fa7533f30aedf06c
ngobiea <ngobiea@gmail.com>
2023-11-23 12:47:39 +0500

    testing full app

--- src/renderer/src/pages/ReportPages/ReportPage.js
+++ src/renderer/src/pages/ReportPages/ReportPage.js
@@ -1,7 +1,24 @@
-import React from "react";
-
+import React from 'react';
+import axios from 'axios';
 const ReportPage = () => {
-
+  const handleDownload = async () => {
+    try {
+      const response = await axios.get('http://localhost:6001/report', {
+        responseType: 'blob',
+      });
+      console.log(response);
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', 'users_report.xlsx');
+      document.body.appendChild(link);
+      link.click();
+      link.parentNode.removeChild(link);
+    } catch (error) {
+      console.log('Error downloading report');
+      console.log(error);
+    }
+  };
   return (
     <div className="p-4 sm:ml-64">
       <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
@@ -17,7 +34,12 @@ const ReportPage = () => {
           </div>
         </div>
         <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-          <p className="text-2xl text-gray-400 dark:text-gray-500">Report Page</p>
+          <button
+            onClick={handleDownload}
+            className="text-2xl text-gray-400 dark:text-gray-500"
+          >
+            Report Page
+          </button>
         </div>
         <div className="grid grid-cols-2 gap-4 mb-4">
           <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
--- src/renderer/src/utils/url.js
+++ src/renderer/src/utils/url.js
@@ -1,3 +1,4 @@
-export const baseUrl = 'http://15.206.194.122:6001';
+export const baseUrl = null;
 export const localhost = 'http://localhost:6001';
 //13.127.151.184
+
