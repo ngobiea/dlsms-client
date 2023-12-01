391ba07ebd7122c4f6dbf3bc92e6741a0a3a85bd
ngobiea <ngobiea@gmail.com>
2023-12-01 16:33:15 +0500

    add startup codes

--- src/renderer/src/utils/url.js
+++ src/renderer/src/utils/url.js
@@ -1,3 +1,6 @@
 export const baseUrl = 'http://13.235.74.197:6001';
 export const localhost = 'http://localhost:6001';
 //13.127.151.184
+// npm run watch
+//npx tailwindcss -i ./src/renderer/public/css/input.css -o ./dist/output.css --watch
+// npm start
